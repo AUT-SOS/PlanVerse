5ad7eb82c84752a5c66f6b40a16217065882e61d
unknown <arshia.bahar@aut.ac.ir>
2024-01-28 16:29:20 +0330

    feat(Khoshk): get state handler added

--- khoshk/models/state.go
+++ khoshk/models/state.go
@@ -28,3 +28,11 @@ type StateObject struct {
 type CreateStateResponse struct {
 	StateID int `json:"state_id"`
 }
+
+type GetStateResponse struct {
+	ID              int    `json:"state_id"`
+	ProjectID       int    `json:"project_id"`
+	Title           string `json:"title"`
+	BackGroundColor string `json:"back_ground_color"`
+	AdminAccess     bool   `json:"admin_access"`
+}
