f2d0de2f23ad49a9aad845ef42b2b0abe2d09fb2
unknown <arshia.bahar@aut.ac.ir>
2024-01-30 14:06:34 +0330

    feat(Khoshk): test get user added

--- khoshk/Tests/get_user_test.go
+++ khoshk/Tests/get_user_test.go
@@ -2,8 +2,8 @@ package Tests
 
 import (
 	"PlanVerse/controllers"
-	"PlanVerse/models"
 	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -13,14 +13,17 @@ import (
 
 func TestGetUser(t *testing.T) {
 	server := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/get-user/10", nil)
 	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/get-my-user", nil)
 	ctx := server.NewContext(req, rec)
-	controllers.GetUserHandler(ctx)
-	result := rec.Result()
-	defer result.Body.Close()
-	var user models.GetUserResponse
-	json.Unmarshal(rec.Body.Bytes(), &user)
-	assert.Equal(t, http.StatusOK, result.Status)
-	assert.Equal(t, "arshiabp", user.Username)
+	ctx.Set("user_id", 89)
+	t.Run("should return 200 status ok and 89 for id", func(t *testing.T) {
+		controllers.GetUserIDHandler(ctx)
+		result := rec.Result()
+		defer result.Body.Close()
+		var userID int
+		json.Unmarshal(rec.Body.Bytes(), &userID)
+		assert.Equal(t, fmt.Sprint("200 OK"), result.Status)
+		assert.Equal(t, 89, userID)
+	})
 }
